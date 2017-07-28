1887ab2bcf2e1c82c5d97f8dd40fad70557301dd
Сенин Роман <rastopyr@gmail.com>
2017-07-28 15:24:12 +0300

    initial

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+
+export default () => (
+  <div>
+    <style jsx>{`
+      h1 {
+        font-family: sans-serif;
+        text-align: center;
+        font-weight: 100
+      }
+    `}</style>
+
+    <h1>{'Hello, Kyiv!'}</h1>
+  <img src={'/static/Batman_logo.svg'} alt=""/>
+  </div>
+);
